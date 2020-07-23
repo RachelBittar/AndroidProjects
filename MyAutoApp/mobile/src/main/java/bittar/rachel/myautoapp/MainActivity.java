f194b3c7700c7e56cd17d6ed5b625567a1b816b3
Rachel Bittar <rachelbbittar@gmail.com>
2020-07-22 20:50:55 -0400

    first commit

---
+++ MyAutoApp/mobile/src/main/java/bittar/rachel/myautoapp/MainActivity.java
@@ -0,0 +1,14 @@
+package bittar.rachel.myautoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
